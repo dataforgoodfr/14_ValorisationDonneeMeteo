c47de5fd38ae7edde4c806731da99c95fe339ac1
Charles Aracil <charles.aracil@gmail.com>
2026-01-23 09:32:21 +0100

    Bootstrap very basic VueJS project

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <HelloWorld msg="Valorisation Donnée Météo" />
+  </main>
+</template>
+
+<style>
+#app {
+  font-family: system-ui, -apple-system, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
