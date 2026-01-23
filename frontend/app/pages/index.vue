f3ca07dc3e6b99df91f121262b4ca3bd29a8584f
rnoyer <romain.noyer@gmail.com>
2026-01-23 18:00:01 +0100

    setup basic routing

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,8 @@
+<!-- 
+ Objectifs V0 :
+ - Page d'accueil sans contenu
+ - Dois simplement être wrappé par les Header + Footer (app.vue)
+-->
+<template>
+  <h1>Accueil Valorisation Météo</h1>
+</template>
