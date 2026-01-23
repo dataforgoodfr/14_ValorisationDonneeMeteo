572f966d2a23ce0bb2b4782970035970ce9f65bc
rnoyer <romain.noyer@gmail.com>
2026-01-23 18:00:01 +0100

    setup basic routing

---
+++ frontend/app/pages/ecartNormale.vue
@@ -0,0 +1,8 @@
+<!-- 
+ Objectifs V0 :
+ - A Réaliser : https://github.com/dataforgoodfr/14_ValorisationDonneeMeteo/issues/7
+-->
+
+<template>
+  <h1>Page Ecart de température à la normale</h1>
+</template>
