66c58ae4ad4815b5fe81f38ca03aba30d97d80d2
Charles Aracil <charles.aracil@gmail.com>
2026-01-28 10:13:53 +0100

    transfer database management to Django

--- backend/weather/admin.py
+++ backend/weather/admin.py
@@ -5,21 +5,21 @@ from .models import HoraireTempsReel, Quotidienne, Station
 
 @admin.register(Station)
 class StationAdmin(admin.ModelAdmin):
-    list_display = ["id", "nom", "departement", "lat", "lon", "alt"]
+    list_display = ["code", "nom", "departement", "lat", "lon", "alt"]
     list_filter = ["departement", "poste_ouvert", "poste_public"]
-    search_fields = ["id", "nom"]
+    search_fields = ["code", "nom"]
     ordering = ["nom"]
 
 
 @admin.register(HoraireTempsReel)
 class HoraireTempsReelAdmin(admin.ModelAdmin):
-    list_display = ["geo_id_insee", "validity_time", "t", "u", "ff", "rr1"]
-    list_filter = ["geo_id_insee"]
+    list_display = ["station", "validity_time", "t", "u", "ff", "rr1"]
+    list_filter = ["station"]
     ordering = ["-validity_time"]
 
 
 @admin.register(Quotidienne)
 class QuotidienneAdmin(admin.ModelAdmin):
-    list_display = ["num_poste", "nom_usuel", "date", "tn", "tx", "rr"]
-    list_filter = ["num_poste"]
+    list_display = ["station", "nom_usuel", "date", "tn", "tx", "rr"]
+    list_filter = ["station"]
     ordering = ["-date"]
