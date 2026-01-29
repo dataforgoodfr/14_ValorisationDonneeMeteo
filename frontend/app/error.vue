241d3c8fd2bcb695545b6b6eaacae093ab913805
Romain Noyer <36259558+rnoyer@users.noreply.github.com>
2026-01-29 18:11:31 +0100

    Basic nuxt structure (#34)
    * update page names
    * add error page header and footer component

---
+++ frontend/app/error.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{ error: NuxtError }>()
+
+</script>
+
+<template>
+    <UError
+    :error="error"
+  />
+</template>
