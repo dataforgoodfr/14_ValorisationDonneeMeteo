9623b4ad647b2198422357d666f10b4e49388cec
Romain Noyer <36259558+rnoyer@users.noreply.github.com>
2026-01-29 18:11:31 +0100

    Basic nuxt structure (#34)
    * update page names
    * add error page header and footer component

---
+++ frontend/app/components/layout/AppFooter.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UFooter>
+    <template #left>
+      <p class="text-muted text-sm">
+        Copyright © {{ new Date().getFullYear() }}
+      </p>
+    </template>
+  </UFooter>
+</template>
---
+++ frontend/app/components/layout/AppHeader.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const route = useRoute();
+
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: "Températures",
+    icon: "material-symbols:thermostat",
+    children: [
+      {
+        label: "Ecarts à la normale",
+        description: "Une description courte de la page",
+        to: "/ecart-normale",
+        icon: "material-symbols:arrow-range-rounded",
+        active: route.path.startsWith("/ecart-normale"),
+      },
+      {
+        label: "Indicateurs thermique",
+        description: "Une description courte de la page",
+        to: "/indicateurs-thermique",
+        icon: "material-symbols:thermostat-arrow-up",
+        active: route.path.startsWith("/indicateurs-thermique"),
+      },
+    ],
+  },
+  {
+    disabled: true,
+    label: "Pluviométrie",
+    icon: "material-symbols:rainy-outline",
+    children: [
+      {
+        //To be filled
+      },
+      {
+        //To be filled
+      },
+    ],
+  },
+]);
+</script>
+
+<template>
+  <UHeader>
+    <template #title>
+      <h1>Infoclimat Dashboard</h1>
+    </template>
+
+    <template #default>
+      <UNavigationMenu content-orientation="vertical" :items="items" />
+    </template>
+
+    <template #right>
+      <UTooltip text="Accedez au site InfoClimat">
+        <UButton
+          color="neutral"
+          variant="outline"
+          label="Tout InfoClimat"
+          to="https://www.infoclimat.fr/"
+          target="_blank"
+          aria-label="infoclimat"
+        />
+      </UTooltip>
+    </template>
+  </UHeader>
+</template>
