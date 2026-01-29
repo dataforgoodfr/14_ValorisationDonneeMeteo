14d8c3899b63de3cd15e682f5e0a4e2addaf72bf
rnoyer <romain.noyer@gmail.com>
2026-01-29 15:03:17 +0100

    update page names

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import AppFooter from './components/layout/AppFooter.vue';
+import AppHeader from './components/layout/AppHeader.vue';
+
+</script>
+
 <template>
   <UApp>
+    <AppHeader />
     <NuxtPage />
+    <AppFooter />
   </UApp>
 </template>
---
+++ frontend/app/components/layout/AppHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Header</h1>
+
+  <UButton icon="i-lucide-rocket" size="md" color="primary" variant="solid">Button</UButton>
+
+</template>
