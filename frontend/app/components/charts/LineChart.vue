78dc0c1ab2f909d341baa2c07581c77bd9264e9c
Romain Noyer <36259558+rnoyer@users.noreply.github.com>
2026-02-09 12:28:50 +0100

    [Feat](front) Add echart lib (#49)
    * feat:add echarts lib
    
    * feat: add echart for nuxt
    
    * basic nuxt config for line and bar charts
    
    * chart example

---
+++ frontend/app/components/charts/LineChart.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>linechart</p>
+  <VChart :option="option" autoresize />
+</template>
+
+<script setup lang="ts">
+import { provide } from "vue";
+
+// provide init-options
+const renderer = ref<"svg" | "canvas">("svg");
+const initOptions = computed(() => ({
+  height: 300,
+  renderer: renderer.value,
+}));
+provide(INIT_OPTIONS_KEY, initOptions);
+
+const option = ref<ECOption>({
+  dataset: {
+    dimensions: ["Year", "Matcha Latte"],
+    source: [
+      { Year: "2015", "Matcha Latte": 54 },
+      { Year: "2016", "Matcha Latte": 42 },
+      { Year: "2017", "Matcha Latte": 23 },
+    ],
+  },
+  xAxis: { type: "category" },
+  yAxis: {},
+  series: [{ type: "line" }],
+});
+</script>
--- frontend/app/pages/ecart-normale.vue
+++ frontend/app/pages/ecart-normale.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import Barchart from "~/components/charts/Barchart.vue";
+import LineChart from "~/components/charts/LineChart.vue";
+
+const items = ref(["Bar Chart", "Line Chart"]);
+const value = ref("Bar Chart");
+</script>
+
 <!-- 
  Objectifs V0 :
  - A Réaliser : https://github.com/dataforgoodfr/14_ValorisationDonneeMeteo/issues/7
@@ -5,6 +13,9 @@
 
 <template>
   <UContainer>
-    <h1>Page Ecart de température à la normale</h1>
+    <h1 class="my-8">Page Ecart de température à la normale</h1>
+    <USelectMenu v-model="value" :items="items" />
+    <Barchart v-if="value === `Bar Chart`" />
+    <LineChart v-if="value === `Line Chart`" />
   </UContainer>
 </template>
