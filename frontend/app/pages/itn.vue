53e816dbdcdcdd97160e588bc87634007e57e064
sbsrouteur <11095871+sbsrouteur@users.noreply.github.com>
2026-02-12 11:31:39 +0100

    Feat/#55 graphe combiné (#73)
    * WIP Composed graph
    
    * Remove traces & dead code
    
    * fix: update code to modern TS
    
    ---------
    
    Co-authored-by: rnoyer <romain.noyer@gmail.com>

--- frontend/app/pages/itn.vue
+++ frontend/app/pages/itn.vue
@@ -3,10 +3,15 @@
  - A Réaliser : https://github.com/dataforgoodfr/14_ValorisationDonneeMeteo/issues/4
 -->
 
-<script setup lang="ts"></script>
+
+
+<script setup lang="ts">
+  import CombinedChart from "~/components/charts/CombinedChart.vue";
+</script>
 
 <template>
   <UContainer>
     <h1>Page Indicateur Thermique National</h1>
+    <CombinedChart />
   </UContainer>
 </template>
