9348c3e385e0c52a15def5bcf374de0005a611e0
nor5 <noorbelkacem5@gmail.com>
2026-02-12 14:42:11 +0100

    feat: setup build and push to GHCR
    # Conflicts:
    #	frontend/app/pages/indicateurs-thermique.vue
    #	frontend/nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,33 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: "2025-07-15",
-    devtools: { enabled: true },
+  ssr: true, //false : use client-side only rendering to generate .output/public/index.html
+  compatibilityDate: "2025-07-15",
+  runtimeConfig: {
 
-    modules: [
-        "@nuxt/eslint",
-        "@nuxt/test-utils",
-        "@nuxt/ui",
-        "@nuxt/image",
-        "@nuxt/fonts",
-        "nuxt-echarts",
-    ],
-    css: ["~/assets/css/main.css"],
-    ui: {
-        colorMode: false,
+    public: {
+      apiBase: '' // api url will be injected when the container is launched with an env variable
+    }
+  },
+  devtools: { enabled: false }, // false for production
+  // couche de compilation runtime genere un index static pour  nginx
+  nitro: {
+  preset: 'node-server'
+   },
+
+  modules: ["@nuxt/test-utils", "@nuxt/ui", "@nuxt/image", '@nuxt/fonts'],
+  css: ['~/assets/css/main.css'],
+  ui: {
+    colorMode: false
+  },
+  app: {
+    head: {
+      title: 'InfoClimat - Dashboard',
+      htmlAttrs: {
+        lang: 'fr',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
     },
-    app: {
-        head: {
-            title: "InfoClimat - Dashboard",
-            htmlAttrs: {
-                lang: "fr",
-            },
-            link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-        },
-    },
-    echarts: {
-        renderer: ["svg", "canvas"],
-        charts: ["BarChart", "LineChart"],
-        components: ["DatasetComponent", "GridComponent", "TooltipComponent"],
-        features: ["LabelLayout", "UniversalTransition"],
-    },
+  },
+
 });
