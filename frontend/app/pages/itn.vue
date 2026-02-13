23d012979443897ba2797b955df4f30f35e26ec5
rnoyer <romain.noyer@gmail.com>
2026-02-13 20:44:54 +0100

    chore: remove commented code

--- frontend/app/pages/itn.vue
+++ frontend/app/pages/itn.vue
@@ -5,41 +5,13 @@
 
 <script setup lang="ts">
 import CombinedChart from "~/components/charts/CombinedChart.vue";
-
-// TODO : replace 'granularity' with dropdown; V0 is : 'Month' only, 'Day' and 'Year' disabled
-// const params = ref({
-//   date_start: "1990-01-01",
-//   date_end: "2024-12-31",
-//   granularity: "day" as const,
-// });
-
-// TODO : uncomment below when "useNationalIndicator" is working
-// const { data, status, error } = await useNationalIndicator(params);
+import selectBar from "~/components/ui/itn/selectBar.vue";
 </script>
 
 <template>
     <UContainer>
         <h1>Page Indicateur Thermique National</h1>
+        <selectBar />
         <CombinedChart />
-        <!-- TODO : Implement template below when API "useNationalIndicator" is ready-->
-        <!-- <div
-      v-if="status === 'pending'"
-      class="flex items-center justify-center py-12"
-    >
-      <UIcon name="i-lucide-loader-circle" class="animate-spin size-6" />
-      <span class="ml-2">Chargement des données...</span>
-    </div>
-
-    <UAlert
-      v-else-if="error"
-      color="error"
-      title="Erreur de chargement"
-      :description="error.message"
-    />
-
-    <CombinedChart
-      v-else-if="data?.time_series"
-      :time-series="data.time_series"
-    /> -->
     </UContainer>
 </template>
