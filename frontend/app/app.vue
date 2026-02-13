919e360eca61347918190e14c3498fe05e339c13
Romain Noyer <36259558+rnoyer@users.noreply.github.com>
2026-02-13 09:31:04 +0100

    feat: set app to french locale (#76)

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import AppFooter from "./components/layout/AppFooter.vue";
 import AppHeader from "./components/layout/AppHeader.vue";
+import { fr } from "@nuxt/ui/locale";
 </script>
 
 <template>
-  <UApp class="min-h-screen flex flex-col">
+  <UApp :locale="fr" class="min-h-screen flex flex-col">
     <AppHeader />
 
     <UMain class="flex-1">
