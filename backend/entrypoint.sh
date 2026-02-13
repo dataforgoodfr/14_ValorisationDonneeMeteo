6cb9deb0fd7cc182e0674dee5e8bba17e5f8c470
nor5 <noorbelkacem5@gmail.com>
2026-02-13 15:57:34 +0100

    fix pre-commit

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,8 +8,4 @@ uv run python manage.py migrate --noinput
 echo "Collecting static files..."
 uv run python manage.py collectstatic --noinput
 
-<<<<<<< HEAD
 exec "$@"
-=======
-exec "$@"
->>>>>>> 9348c3e (feat: setup build and push to GHCR)
