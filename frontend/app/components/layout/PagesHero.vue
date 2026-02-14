ba0549d029a5c01d05d4cc185d1ebddb7abf54de
rnoyer <romain.noyer@gmail.com>
2026-02-14 15:08:15 +0100

    feat: Add Header

---
+++ frontend/app/components/layout/PagesHero.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+});
+</script>
+
+<template>
+    <UPageHero :title="title" :description="description" />
+</template>
