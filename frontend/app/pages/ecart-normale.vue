30991be02cdce11bda7fe13dd25087bd95b112bc
Romain Noyer <36259558+rnoyer@users.noreply.github.com>
2026-02-14 15:09:34 +0100

    feat: Add Header (#83)

--- frontend/app/pages/ecart-normale.vue
+++ frontend/app/pages/ecart-normale.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import PagesHero from "~/components/layout/PagesHero.vue";
 import Barchart from "~/components/charts/Barchart.vue";
 import LineChart from "~/components/charts/LineChart.vue";
+
+const heroData = {
+    title: "Ecart à la normale",
+    description:
+        'L\'écart à la normale est la différence entre la moyenne des températures sur une période, et les températures normales. Les " normales " sont calculées sur 30 ans et mises à jour toutes les décennies.',
+};
 
 const items = ref(["Bar Chart", "Line Chart"]);
 const value = ref("Bar Chart");
@@ -13,7 +20,10 @@ const value = ref("Bar Chart");
 
 <template>
     <UContainer>
-        <h1 class="my-8">Page Ecart de température à la normale</h1>
+        <PagesHero
+            :title="heroData.title"
+            :description="heroData.description"
+        />
         <USelectMenu v-model="value" :items="items" />
         <Barchart v-if="value === `Bar Chart`" />
         <LineChart v-if="value === `Line Chart`" />
