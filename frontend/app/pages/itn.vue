fcc475b910af28e113797729c34cd284592099f8
rnoyer <romain.noyer@gmail.com>
2026-02-16 01:12:47 +0100

    chore: rebase

--- frontend/app/pages/itn.vue
+++ frontend/app/pages/itn.vue
@@ -6,19 +6,13 @@
 <script setup lang="ts">
 import CombinedChart from "~/components/charts/CombinedChart.vue";
 import PagesHero from "~/components/layout/PagesHero.vue";
+import SelectBar from "~/components/ui/itn/selectBar.vue";
 
 const heroData = {
     title: "ITN",
     description:
         "L'Indicateur Thermique National est la température moyenne mesurée à l'échelle du pays (moyenne des températures minimales et maximales sur 30 stations météorologiques réparties de manière équilibrée en France).",
 };
-
-// TODO : replace 'granularity' with dropdown; V0 is : 'Month' only, 'Day' and 'Year' disabled
-// const params = ref({
-//   date_start: "1990-01-01",
-//   date_end: "2024-12-31",
-//   granularity: "day" as const,
-// });
 
 // TODO : uncomment below when "useNationalIndicator" is working
 // const { data, status, error } = await useNationalIndicator(params);
@@ -30,6 +24,7 @@ const heroData = {
             :title="heroData.title"
             :description="heroData.description"
         />
+        <SelectBar />
         <CombinedChart />
     </UContainer>
 </template>
