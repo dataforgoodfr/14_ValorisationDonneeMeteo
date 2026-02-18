8656d681cc4557f864b4ed07291cc5ae6636a70c
Amélie Rondot <amelie.rondot@multi.coop>
2026-02-18 09:57:06 +0100

    feat: introduce intermediar function 'formatFileName'

--- frontend/app/components/ui/itn/ExportPng.vue
+++ frontend/app/components/ui/itn/ExportPng.vue
@@ -5,6 +5,27 @@
 <script setup lang="ts">
 import * as echarts from 'echarts';
 
+function formatDateManually(date) {
+  // Helper function to pad numbers with a leading zero
+  const pad = (num) => num.toString().padStart(2, '0');
+
+  const year = date.getFullYear();
+  const month = pad(date.getMonth() + 1); // getMonth() is zero-based (0-11)
+  const day = pad(date.getDate());
+
+  const hours = pad(date.getHours());
+  const minutes = pad(date.getMinutes());
+  const seconds = pad(date.getSeconds());
+
+  return `${year}${month}${day}_${hours}${minutes}${seconds}`;
+};
+
+function formatFileName(xUnit: string, dt: string){
+    const now = new Date(Date.now());
+    // Question : date UTC ou locale ??
+    return `${formatDateManually(now)}_IndicateurThermiqueNational_${xUnit}_${dt}`
+};
+
 async function exportPngFnc(){
     const img = new Image();
     const chart = echarts.getInstanceByDom(document.getElementById('itnCombinedChart'));
@@ -22,7 +43,8 @@ async function exportPngFnc(){
 
     const anchorElement = document.createElement('a');
     anchorElement.href = href;
-    anchorElement.download = 'YYYYMMDD_HHMMSS_IndicateurThermiqueNational_XUnit_DT';
+    // ATTENTION : l'intervalle de temps est codé en dur !
+    anchorElement.download = formatFileName('mois', '20251001_to_20251231');
 
     document.body.appendChild(anchorElement);
     anchorElement.click();
