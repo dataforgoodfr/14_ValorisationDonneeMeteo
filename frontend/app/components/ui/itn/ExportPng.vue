f7a2a78cd95d588e298d826c723265322803a330
Amélie Rondot <amelie.rondot@multi.coop>
2026-02-18 08:56:21 +0100

    WIP: try to add an Export PNG button to export graph at PNG format

---
+++ frontend/app/components/ui/itn/ExportPng.vue
@@ -0,0 +1,22 @@
+<template>
+    <UButton id="exportPngButton" color="neutral" @click="exportPngFnc">Export en png</UButton>
+</template>
+
+<script setup lang="ts">
+import * as echarts from 'echarts';
+
+function exportPngFnc(){
+    console.log("export PNG")
+    const img = new Image();
+    console.log(document.getElementById('itn_combined_chart'));
+    const chart = echarts.getInstanceByDom(document.getElementById('itnCombinedChart'));
+    img.src = chart.getDataURL({
+        type:"png",
+        pixelRatio: 2,
+        backgroundColor: '#fff'
+    });
+    console.log(img.src)
+
+}
+
+</script>
