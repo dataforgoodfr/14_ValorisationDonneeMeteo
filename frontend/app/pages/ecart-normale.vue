109cf9baf7f4861a90db70cb0e2e192dc9263294
rnoyer <romain.noyer@gmail.com>
2026-02-26 12:14:56 +0100

    feat: add another map leveraging Nuxt module simplicity

--- frontend/app/pages/ecart-normale.vue
+++ frontend/app/pages/ecart-normale.vue
@@ -3,7 +3,8 @@ import PagesHero from "~/components/layout/PagesHero.vue";
 import Barchart from "~/components/charts/Barchart.vue";
 import LineChart from "~/components/charts/LineChart.vue";
 import StationTable from "~/components/ui/ecartNormale/stationTable.vue";
-import SimpleMapClient from "~/components/charts/SimpleMap.client.vue";
+import SimpleMapClient from "~/components/charts/SimpleMap.vue";
+import SimpleMapAnotherClient from "~/components/charts/SimpleMapAnother.vue";
 
 const heroData = {
     title: "Ecart à la normale",
@@ -29,6 +30,7 @@ const value = ref("Bar Chart");
         <USelect v-model="value" :items="items" />
         <Barchart v-if="value === `Bar Chart`" />
         <LineChart v-if="value === `Line Chart`" />
+        <SimpleMapAnotherClient />
         <SimpleMapClient />
         <StationTable />
     </UContainer>
