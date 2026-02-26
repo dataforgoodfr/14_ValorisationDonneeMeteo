b60323b966baf148e0745ad5001d878ed362138b
rnoyer <romain.noyer@gmail.com>
2026-02-26 23:36:20 +0100

    feat: wip manage form and type de moyenne

--- frontend/app/components/ui/itn/monthRangeDatePicker.vue
+++ frontend/app/components/ui/itn/monthRangeDatePicker.vue
@@ -1,45 +1,51 @@
 <script setup lang="ts">
+import type { CalendarDate } from "@internationalized/date";
 import { useCustomDate } from "~/composables/useCustomDate";
 
 const date = useCustomDate();
-const startMonth = shallowRef(date.lastYearYYYYMD.value);
-const endMonth = shallowRef(date.twoDaysAgoYYYMD.value);
+const startMonth = defineModel<CalendarDate>("startMonth", { required: true }); // date.lastYearYYYYMD.value
+const endMonth = defineModel<CalendarDate>("endMonth", { required: true }); // date.twoDaysAgoYYYMD.value
 </script>
 
 <template>
     <div class="flex items-center gap-1">
-        <UPopover>
-            <UButton color="neutral" variant="outline">
-                {{ date.monthYearDisplay(startMonth) }}
-            </UButton>
-            <template #content>
-                <UCalendar
-                    v-model="startMonth"
-                    class="p-2"
-                    :prevent-deselect="true"
-                    :month-controls="true"
-                    :year-controls="true"
-                    :min-value="date.absoluteMinDataDateYYYYMD"
-                    :max-value="endMonth"
-                />
-            </template>
-        </UPopover>
-        <UIcon name="i-lucide-arrow-right" />
-        <UPopover>
-            <UButton color="neutral" variant="outline">
-                {{ date.monthYearDisplay(endMonth) }}
-            </UButton>
-            <template #content>
-                <UCalendar
-                    v-model="endMonth"
-                    class="p-2"
-                    :prevent-deselect="true"
-                    :month-controls="true"
-                    :year-controls="true"
-                    :min-value="startMonth"
-                    :max-value="date.twoDaysAgoYYYMD.value"
-                />
-            </template>
-        </UPopover>
+        <UFormField label="Date de début" name="date_start">
+            <UPopover>
+                <UButton color="neutral" variant="outline">
+                    {{ date.monthYearDisplay(startMonth) }}
+                </UButton>
+                <template #content>
+                    <UCalendar
+                        v-model="startMonth"
+                        class="p-2"
+                        :prevent-deselect="true"
+                        :month-controls="true"
+                        :year-controls="true"
+                        :min-value="date.absoluteMinDataDateYYYYMD"
+                        :max-value="endMonth"
+                    />
+                </template>
+            </UPopover>
+        </UFormField>
+        <div class="pt-7"><UIcon name="i-lucide-arrow-right" /></div>
+
+        <UFormField label="Date de fin" name="date_end">
+            <UPopover>
+                <UButton color="neutral" variant="outline">
+                    {{ date.monthYearDisplay(endMonth) }}
+                </UButton>
+                <template #content>
+                    <UCalendar
+                        v-model="endMonth"
+                        class="p-2"
+                        :prevent-deselect="true"
+                        :month-controls="true"
+                        :year-controls="true"
+                        :min-value="startMonth"
+                        :max-value="date.twoDaysAgoYYYMD.value"
+                    />
+                </template>
+            </UPopover>
+        </UFormField>
     </div>
 </template>
--- frontend/app/components/ui/itn/selectBar.vue
+++ frontend/app/components/ui/itn/selectBar.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
+import { useCustomDate } from "#imports";
 import monthRangeDatePicker from "./monthRangeDatePicker.vue";
+import type { CalendarDate } from "@internationalized/date";
+
+const date = useCustomDate();
+const selectState = reactive({
+    date_start: date.lastYearYYYYMD.value,
+    date_end: date.twoDaysAgoYYYMD.value,
+    granularity: "month" as "year" | "month" | "day",
+    slice_type: undefined as
+        | undefined
+        | "full"
+        | "month_of_year"
+        | "day_of_month",
+    month_of_year: undefined as undefined | number,
+    day_of_month: undefined as undefined | number,
+});
 
 // Granularity Selection
 const granularity = ref([
@@ -7,12 +23,64 @@ const granularity = ref([
     { label: "Mois", value: "month" },
     { label: "Année", value: "year", disabled: true },
 ]);
-const granularityValue = ref("Mois");
+
+const isMeanType = ref(false);
+
+const meanType = ref([
+    { label: "Jour", value: "day" },
+    { label: "Mois", value: "month" },
+    { label: "Année", value: "year" },
+]);
+const meanTypeValue = ref("");
 </script>
 
 <template>
-    <div class="flex gap-6">
-        <USelect v-model="granularityValue" :items="granularity" />
-        <monthRangeDatePicker />
+    <div class="flex gap-6 px-3 py-2">
+        <div id="main-filter" class="flex gap-6">
+            <UFormField label="Granularité" name="granularity">
+                <USelect
+                    v-model="selectState.granularity"
+                    :items="granularity"
+                    name="granularity"
+                />
+            </UFormField>
+
+            <monthRangeDatePicker
+                :start-month="selectState.date_start"
+                :end-month="selectState.date_end"
+            />
+            <USeparator
+                orientation="vertical"
+                class="w-px bg-gray-200 self-stretch"
+            />
+            <UButton type="submit"> Rafraichir </UButton>
+
+            <USeparator
+                orientation="vertical"
+                class="w-px bg-gray-200 self-stretch"
+            />
+            <USwitch
+                v-model="isMeanType"
+                unchecked-icon="i-lucide-x"
+                checked-icon="i-lucide-check"
+                label="Type de moyenne"
+                :ui="{
+                    root: 'flex-col justify-between text-center items-center',
+                    container: 'my-auto',
+                }"
+            />
+
+            <UFormField
+                v-if="isMeanType"
+                label="Type de moyenne"
+                name="slice_type"
+            >
+                <USelect
+                    v-model="meanTypeValue"
+                    placeholder="Type de moyenne"
+                    :items="meanType"
+                />
+            </UFormField>
+        </div>
     </div>
 </template>
