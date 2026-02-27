f9c6bdc03a8eb38ebf8f102849f971eb9d6823fe
Amélie Rondot <amelie.rondot@multi.coop>
2026-02-27 13:49:22 +0100

    refacto: factorize code by introducing utils public module

---
+++ frontend/public/utils.ts
@@ -0,0 +1,20 @@
+export function formatDateManually(date: Date) {
+    // Helper function to pad numbers with a leading zero
+    const pad = (num: number) => num.toString().padStart(2, "0");
+
+    const year = date.getFullYear();
+    const month = pad(date.getMonth() + 1); // getMonth() is zero-based (0-11)
+    const day = pad(date.getDate());
+
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    const seconds = pad(date.getSeconds());
+
+    return `${year}${month}${day}_${hours}${minutes}${seconds}`;
+}
+
+export function formatFileName(xUnit: string, dt: string) {
+    // Helper function to format exported file name
+    const now = new Date(Date.now());
+    return `${formatDateManually(now)}_IndicateurThermiqueNational_${xUnit}_${dt}`;
+}
