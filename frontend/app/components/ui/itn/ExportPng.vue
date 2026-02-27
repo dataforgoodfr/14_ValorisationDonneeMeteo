8e0f230f6932c3db830923b22723ec5319fb7b1a
Amélie Rondot <amelie.rondot@multi.coop>
2026-02-27 13:49:22 +0100

    refacto: factorize code by introducing utils public module

--- frontend/app/components/ui/itn/ExportPng.vue
+++ frontend/app/components/ui/itn/ExportPng.vue
@@ -6,27 +6,7 @@
 
 <script setup lang="ts">
 import * as echarts from "echarts";
-
-function formatDateManually(date) {
-    // Helper function to pad numbers with a leading zero
-    const pad = (num: number) => num.toString().padStart(2, "0");
-
-    const year = date.getFullYear();
-    const month = pad(date.getMonth() + 1); // getMonth() is zero-based (0-11)
-    const day = pad(date.getDate());
-
-    const hours = pad(date.getHours());
-    const minutes = pad(date.getMinutes());
-    const seconds = pad(date.getSeconds());
-
-    return `${year}${month}${day}_${hours}${minutes}${seconds}`;
-}
-
-function formatFileName(xUnit: string, dt: string) {
-    const now = new Date(Date.now());
-    // Question : date UTC ou locale ??
-    return `${formatDateManually(now)}_IndicateurThermiqueNational_${xUnit}_${dt}`;
-}
+import { formatFileName } from "~~/public/utils";
 
 async function exportPngFnc() {
     const img = new Image();
