9393ecee9b72454e64f486365493f385e76ce2db
MarkJoly <mark.joly@gmail.com>
2026-03-02 16:09:41 +0100

    feat(sql): create SQL views: station, quotidienne

---
+++ backend/sql/views/002_v_quotidienne.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE VIEW public.v_quotidienne_itn AS
+SELECT
+  (q."NUM_POSTE"::text || '_' || to_char(q."AAAAMMJJ", 'YYYYMMDD')) AS id,
+  q."NUM_POSTE"::text AS station_code,
+  q."AAAAMMJJ"::date  AS date,
+  q."TNTXM"           AS tntxm
+FROM public."Quotidienne" q
+WHERE q."TNTXM" IS NOT NULL;
