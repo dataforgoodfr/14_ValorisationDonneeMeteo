61233bc866563abc491c86effdee8a3a98d2e0a0
jlecordier <jeanlecordier@hotmail.fr>
2026-03-24 13:49:06 +0100

    ci/cd: front: add vitest config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -5,7 +5,35 @@ export default defineConfig({
     test: {
         globals: true,
         environment: "node",
-        include: ["app/adapters/**/*.test.ts"],
+        projects: [
+            {
+                extends: true,
+                test: {
+                    name: "unit",
+                    include: ["app/**/*.test.ts"],
+                    exclude: ["**/*.integration.test.ts", "**/*.e2e.test.ts"],
+                    sequence: {
+                        concurrent: true,
+                    },
+                },
+            },
+            {
+                extends: true,
+                test: {
+                    name: "integration",
+                    include: ["app/**/*.integration.test.ts"],
+                    testTimeout: 1 * 60 * 1000,
+                },
+            },
+            {
+                extends: true,
+                test: {
+                    name: "e2e",
+                    include: ["app/**/*.e2e.test.ts"],
+                    testTimeout: 1 * 60 * 1000,
+                },
+            },
+        ],
     },
     resolve: {
         alias: {
