9fba46c6eaed9c984e650cdcf907f3d0a1237fd6
Romain Noyer <36259558+rnoyer@users.noreply.github.com>
2026-03-24 14:36:27 +0100

    Fix/front merge to main (#192)
    * chore: ignore mocked data
    
    * chore: delete no more used chart
    
    * fix: end date picker
    
    * fix: new way to display selectBar

--- frontend/app/pages/itn.vue
+++ frontend/app/pages/itn.vue
@@ -1,14 +1,11 @@
-<!--
- Objectifs V0 :
- - A Réaliser : https://github.com/dataforgoodfr/14_ValorisationDonneeMeteo/issues/4
--->
-
 <script setup lang="ts">
-// import CombinedChart from "~/components/charts/CombinedChart.vue";
 import ItnChart from "~/components/charts/ItnChart.vue";
 import PagesHero from "~/components/layout/PagesHero.vue";
 import SelectBar from "~/components/ui/commons/selectBar/selectBar.vue";
 import ChartLayout from "~/components/layout/ChartLayout.vue";
+import { useItnSelectBarAdapter } from "~/adapters/itnSelectBarAdapter";
+
+const selectBarAdapter = useItnSelectBarAdapter();
 
 const heroData = {
     title: "ITN",
@@ -25,10 +22,10 @@ const heroData = {
         />
         <ChartLayout>
             <template #select-bar>
-                <SelectBar />
+                <SelectBar :adapter="selectBarAdapter" />
             </template>
             <template #chart>
-                <ItnChart class="px-3 py-2" />
+                <ItnChart :adapter="selectBarAdapter" class="px-3 py-2" />
             </template>
         </ChartLayout>
     </UContainer>
