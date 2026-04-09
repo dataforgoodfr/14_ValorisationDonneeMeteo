ea6d24bb0e5ea27f3504f79d2ffea8202a250d59
rnoyer <romain.noyer@gmail.com>
2026-04-09 11:04:07 +0200

    feat:enable default dark mode

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -2,6 +2,10 @@
 import AppFooter from "./components/layout/AppFooter.vue";
 import AppHeader from "./components/layout/AppHeader.vue";
 import { fr } from "@nuxt/ui/locale";
+
+const colorMode = useColorMode();
+colorMode.value = "dark";
+colorMode.preference = "dark";
 </script>
 
 <template>
