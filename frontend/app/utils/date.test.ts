6ef4799995ad4335270d133507f3b1320540639c
rnoyer <romain.noyer@gmail.com>
2026-04-09 22:56:39 +0200

    fix: date tests

--- frontend/app/utils/date.test.ts
+++ frontend/app/utils/date.test.ts
@@ -2,8 +2,8 @@ import { describe, test, expect } from "vitest";
 import { dateToStringDMY, dateToStringYMD } from "./date";
 
 describe("Date", () => {
-    const firstDayDateToConvert = new Date("Jan 01 2025 00:00:00 GMT+0100");
-    const lastDayDateToConvert = new Date("Dec 31 2025 00:00:00 GMT+0100");
+    const firstDayDateToConvert = new Date("Jan 01 2025 00:00:00");
+    const lastDayDateToConvert = new Date("Dec 31 2025 00:00:00");
 
     test("is converted to 'YYYY-MM-DD' string", () => {
         const convertedDate = dateToStringYMD(firstDayDateToConvert);
