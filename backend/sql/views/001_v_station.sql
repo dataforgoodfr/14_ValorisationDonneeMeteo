e2f89372571362ebba1cf3e4d909a6ee0146c45b
Tchouanga <ftchouanga@gmail.com>
2026-04-13 02:54:32 +0200

    feat:completeness:ajout des colonnes annee_de_creation et annee_de_fermeture à la vue station.

--- backend/sql/views/001_v_station.sql
+++ backend/sql/views/001_v_station.sql
@@ -9,7 +9,11 @@ SELECT DISTINCT ON (s."id")
   s."lat" AS lat,
   s."alt" AS alt,
   s."postePublic" AS is_public,
+  scd."annee_de_creation" AS annee_de_creation,
+  scd."annee_de_fermeture" AS annee_de_fermeture,
   s."createdAt" AS created_at,
   s."updatedAt" AS updated_at
 FROM public."Station" s
+LEFT JOIN public."station_creation_date" scd
+ ON s."id" = scd."station_code"
 ORDER BY s."id", s."frequence";
