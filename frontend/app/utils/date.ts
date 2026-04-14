0b5af7283b4d16f1e4218c456d9d4317451cdafb
AntoineFederici <antoinefederici.af@gmail.com>
2026-04-14 19:22:12 +0200

    add calendar graph from main branch (#290)
    * add calendar graph from main branch
    
    * Fix: typing / new dates utils functions
    
    * deviationChartTooltipFormatter removed mock
    
    * colors: semantic naming (foreground)
    
    * getStationById: fixed params name
    
    * add function, foreground and improve import
    
    * supprime redondance
    
    * import ECchart pour gérer différent type de grpahe
    
    * adapte Y et X label selon granularité
    
    ---------
    
    Co-authored-by: Myriam <mrabhi@corp.free.fr>

--- frontend/app/utils/date.ts
+++ frontend/app/utils/date.ts
@@ -3,7 +3,44 @@ export function dateToStringYMD(date: Date): string {
     return `${date.toLocaleDateString("sv-SE")}`;
 }
 
+/** Returns the first day of the year for the given date as YYYY-01-01 (local time) */
+export function dateToFirstDayOfYearYMD(date: Date = new Date()): string {
+    return dateToStringYMD(new Date(date.getFullYear(), 0, 1));
+}
+
+/**
+ * Returns the last day of the year for the given date as YYYY-12-31 (local time)
+ * otherwise, returns today minus 2 days if the year is the current year.
+ */
+export function dateToLastDayOfYearYMD(date: Date = new Date()): string {
+    return dateToStringYMD(setToLastDayOfYear(date));
+}
+
 /** Format a Date as DD/MM/YYYY for display. */
 export function dateToStringDMY(date: Date): string {
     return `${date.toLocaleDateString("fr-FR")}`;
+}
+
+/** Returns the first day of the year for the given date as date */
+export function setToFirstDayOfYear(date: Date): Date {
+    return new Date(date.getFullYear(), 0, 1, 0, 0, 0, 0);
+}
+
+/**
+ * Returns the last day of the year for the given date as date (local time)
+ * otherwise, returns today minus 2 days if the year is the current year.
+ */
+export function setToLastDayOfYear(date: Date = new Date()): Date {
+    const today = new Date();
+    const isCurrentYear = date.getFullYear() === today.getFullYear();
+
+    if (isCurrentYear) {
+        const lastDay = new Date(today);
+        lastDay.setDate(today.getDate() - 2);
+        lastDay.setHours(23, 59, 59, 999);
+
+        return lastDay;
+    }
+
+    return new Date(date.getFullYear(), 11, 31, 23, 59, 59, 999);
 }
