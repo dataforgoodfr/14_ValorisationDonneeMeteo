09d23dd45c035edc1cdaada965216e460f3e8985
Myriam <mrabhi@corp.free.fr>
2026-04-14 12:35:49 +0200

    getStationById: fixed params name

--- frontend/app/utils/station.ts
+++ frontend/app/utils/station.ts
@@ -2,9 +2,7 @@ import type { DeviationStationIdAndName } from "~/types/common";
 
 export const getStationById = (
     stations: DeviationStationIdAndName[],
-    station_id: string,
+    stationId: string,
 ): DeviationStationIdAndName | null => {
-    return (
-        stations.find((station) => station.station_id === station_id) || null
-    );
+    return stations.find((station) => station.station_id === stationId) || null;
 };
