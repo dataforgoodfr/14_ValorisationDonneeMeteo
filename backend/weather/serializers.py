6d72ae83cabcd9a20a2eb4a92723c650d2236bef
Thomas W <thomaswatteau1@gmail.com>
2026-04-14 18:35:16 +0200

    feat: add lat lon alt fields to TemperatureRecord entries and serializers

--- backend/weather/serializers.py
+++ backend/weather/serializers.py
@@ -292,6 +292,9 @@ class TemperatureRecordEntrySerializer(serializers.Serializer):
     department = serializers.CharField()
     record_value = serializers.FloatField()
     record_date = serializers.DateField()
+    lat = serializers.FloatField(allow_null=True)
+    lon = serializers.FloatField(allow_null=True)
+    alt = serializers.FloatField(allow_null=True)
 
 
 class TemperatureDeviationOverviewQuerySerializer(serializers.Serializer):
