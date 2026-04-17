ab85e311c32ad3442a6e4be633fadf10bcd49c35
AntoineFede <antoinefederici.af@gmail.com>
2026-04-17 23:31:25 +0200

    tune visualmap color

--- frontend/app/constants/colors.ts
+++ frontend/app/constants/colors.ts
@@ -11,9 +11,11 @@ const deviationMax = 5;
 const deviationStops: [number, string][] = [
     [deviationMin, COLORS.negative],
     [deviationMin * 0.6, "hsl(210, 85%, 75%)"],
-    [deviationMin * 0.2, "hsl(210, 95%, 90%)"],
+    [deviationMin * 0.2, "hsl(180, 90%, 85%)"],
+    [deviationMin * 0.05, "hsl(160, 95%, 90%)"],
     [0, "#ffffff"],
-    [deviationMax * 0.2, "hsl(0, 95%, 90%)"],
+    [deviationMax * 0.05, "hsl(50, 96%, 90%)"],
+    [deviationMax * 0.2, "hsl(30, 90%, 85%)"],
     [deviationMax * 0.6, "hsl(0, 85%, 75%)"],
     [deviationMax, COLORS.positive],
 ];
