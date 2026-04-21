29b11354b911b2e46a3fdd5eb9eb95efe593a687
AntoineFede <antoinefederici.af@gmail.com>
2026-04-21 13:34:18 +0200

    fix test

--- frontend/app/utils/itn/nationalIndicatorFetcher.real.ts
+++ frontend/app/utils/itn/nationalIndicatorFetcher.real.ts
@@ -3,13 +3,12 @@ import type { GranularityType } from "~/components/ui/commons/selectBar/types";
 import { useApiClient } from "~/composables/useApiClient";
 import type { NationalIndicatorForYearFetcher } from "./nationalIndicatorFetcher";
 
-const { apiFetch } = useApiClient();
-
 export const fetchNationalIndicatorForYear: NationalIndicatorForYearFetcher =
     function (
         year: number,
         granularity: GranularityType,
     ): Promise<NationalIndicatorResponse> {
+        const { apiFetch } = useApiClient();
         return apiFetch<NationalIndicatorResponse>(
             "/temperature/national-indicator",
             {
