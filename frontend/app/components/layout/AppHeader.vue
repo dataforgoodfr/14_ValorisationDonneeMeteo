3d656b7331b1ac76d9e2fd1762a7e0784a8e288a
rnoyer <romain.noyer@gmail.com>
2026-04-22 09:14:24 +0200

    chore: comment min-max link in header

--- frontend/app/components/layout/AppHeader.vue
+++ frontend/app/components/layout/AppHeader.vue
@@ -19,13 +19,12 @@ const items = computed<NavigationMenuItem[]>(() => [
                 icon: "i-lucide-thermometer-sun",
                 active: route.path.startsWith("/itn"),
             },
-            {
-                disabled: true,
-                label: "Min/max",
-                to: "/itn",
-                icon: "i-lucide-diff",
-                // active: route.path.startsWith("/itn"),
-            },
+            // {
+            //     label: "Min/max",
+            //     to: "/itn",
+            //     icon: "i-lucide-diff",
+            //     active: route.path.startsWith("/min-max"),
+            // },
             {
                 label: "Records",
                 to: "/records",
