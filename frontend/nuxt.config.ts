8d92cc5bba54eb25cadc92d338f25536f8e382ec
Sophia-San <sophia.senhaji@gmail.com>
2026-04-28 14:44:55 +0200

    fix(mobile): add viewport for ios

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -53,6 +53,12 @@ export default defineNuxtConfig({
             htmlAttrs: {
                 lang: "fr",
             },
+            meta: [
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1",
+                },
+            ],
             link: [
                 { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
                 {
