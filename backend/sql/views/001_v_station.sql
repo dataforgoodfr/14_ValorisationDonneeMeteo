138b6ee2eb5800ffccb23198f984056b630eab7f
jlecordier <jeanlecordier@hotmail.fr>
2026-04-28 13:47:25 +0200

    back: sql: station: filter by typePoste <= 3

--- backend/sql/views/001_v_station.sql
+++ backend/sql/views/001_v_station.sql
@@ -5,21 +5,22 @@ WITH station_classe_recente AS (
     WHERE date_fin IS NULL
 )
 SELECT DISTINCT ON (s."id")
-  s."id" AS station_code,
-  s."nom" AS name,
-  s."departement" AS departement,
-  s."posteOuvert" AS is_open,
-  s."typePoste" AS station_type,
-  s."lon" AS lon,
-  s."lat" AS lat,
-  s."alt" AS alt,
-  s."postePublic" AS is_public,
-  scr.classe AS classe_recente,
-  scd."annee_de_creation" AS annee_de_creation,
-  scd."annee_de_fermeture" AS annee_de_fermeture
+    s."id" AS station_code,
+    s."nom" AS name,
+    s."departement" AS departement,
+    s."posteOuvert" AS is_open,
+    s."typePoste" AS station_type,
+    s."lon" AS lon,
+    s."lat" AS lat,
+    s."alt" AS alt,
+    s."postePublic" AS is_public,
+    scr.classe AS classe_recente,
+    scd."annee_de_creation" AS annee_de_creation,
+    scd."annee_de_fermeture" AS annee_de_fermeture
 FROM public."Station" s
- JOIN public."station_creation_date" scd
-  ON s."id" = scd."station_code"
- LEFT JOIN station_classe_recente scr
-  ON s."id" = scr."station_code"
+    JOIN public."station_creation_date" scd
+        ON s."id" = scd."station_code"
+    LEFT JOIN station_classe_recente scr
+        ON s."id" = scr."station_code"
+WHERE s."typePoste" <= 3
 ORDER BY s."id";
