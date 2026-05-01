7dcb98fa63fb41e5e6f971c008ed12e9ada4eeab
Tchouanga <ftchouanga@gmail.com>
2026-05-01 02:59:09 +0200

    feat:completeness:ajout du fichier permetant de produire les fichiers csv

---
+++ backend/completeness/scripts/csv/extractStationCsv.py
@@ -0,0 +1,30 @@
+import argparse
+
+from stationDataProcessors import *
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Lire un fichier passé en argument de la ligne de commande"
+    )
+    parser.add_argument("file_path", help="Chemin du fichier à lire")
+
+    args = parser.parse_args()
+
+    try:
+        with open(args.file_path, encoding="utf-8") as f:
+            content = f.read()
+    except FileNotFoundError:
+        print(f"Error: File not found -> {args.file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    station_csv = StationDataProcessor(args.file_path)
+    station_csv.extract_data()
+    station_csv.prepare_table_station_classe()
+    station_csv.prepare_table_station_creation_date()
+    station_csv.store_data()
+
+
+if __name__ == "__main__":
+    main()
