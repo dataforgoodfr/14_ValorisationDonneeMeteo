fd9582274c8e4582ca8c6ad6db204907c074767b
AntoineFede <antoinefederici.af@gmail.com>
2026-05-03 19:14:46 +0200

    commente non utilisé card

--- frontend/app/components/home/Last365DaysSection/LastYearSection.vue
+++ frontend/app/components/home/Last365DaysSection/LastYearSection.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import Section from "../Section.vue";
 import GoToDataLink from "../GoToDataLink.vue";
-import RecordsRatioCard from "./RecordsRatioCard.vue";
+// import RecordsRatioCard from "./RecordsRatioCard.vue";
 import Itn365Cards from "./Itn365Cards.vue";
-import ExtremeCard from "../ExtremeCard.vue";
+// import ExtremeCard from "../ExtremeCard.vue";
 
 const { yesterday, yesterdayLess365Days } = useCustomDate();
 </script>
@@ -17,17 +17,18 @@ const { yesterday, yesterdayLess365Days } = useCustomDate();
         </h2>
         <Itn365Cards />
         <GoToDataLink :data-url="'/itn'" />
-        <div class="border-b to-dark-200" />
+        <!-- commenter en attendant l'implémentation -->
+        <!-- <div class="border-b to-dark-200" />
         <h2 class="text-blue-700 dark:text-primary pb-2 pt-1">
             RECORDS DE TEMPERATURE
-        </h2>
-        <div class="flex gap-6 justify-center items-center">
+        </h2> -->
+        <!-- <div class="flex gap-6 justify-center items-center">
             <RecordsRatioCard class="flex-1" />
             <div class="flex flex-col gap-2 w-fit">
                 <ExtremeCard hot-cold="hot" :disabled="true" />
                 <ExtremeCard hot-cold="cold" :disabled="true" />
             </div>
         </div>
-        <GoToDataLink :data-url="'/records'" />
+        <GoToDataLink :data-url="'/records'" /> -->
     </Section>
 </template>
