4383b57bcbf671075dbdcde2cf6616324a581c7e
Thomas W <thomaswatteau1@gmail.com>
2026-05-07 18:05:37 +0200

    feat: update continuously the materialized views for absolute extremes

--- backend/sql/cron/001_mv_quotidienne_realtime.sql
+++ backend/sql/cron/001_mv_quotidienne_realtime.sql
@@ -3,5 +3,12 @@ CREATE EXTENSION IF NOT EXISTS pg_cron;
 SELECT cron.schedule(
    'refresh-mv-quotidienne-realtime',
    '*/6 * * * *',
-   $$REFRESH MATERIALIZED VIEW CONCURRENTLY mv_quotidienne_realtime;$$
+   $$
+   REFRESH MATERIALIZED VIEW CONCURRENTLY mv_quotidienne_realtime;
+   REFRESH MATERIALIZED VIEW mv_itn_daily_all_years;
+   REFRESH MATERIALIZED VIEW mv_itn_daily_all_years_with_feb29;
+   REFRESH MATERIALIZED VIEW mv_itn_absolute_extremes_daily;
+   REFRESH MATERIALIZED VIEW mv_itn_absolute_extremes_monthly;
+   REFRESH MATERIALIZED VIEW mv_itn_absolute_extremes_yearly;
+   $$
 );
