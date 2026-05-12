5585ca40a0aaff15a0a5e5dc3d3d9009ac9513f9
Myriam <mrabhi@corp.free.fr>
2026-05-12 20:50:12 +0200

    Feat: DeviationKpiPanel  ajout etat loading

--- frontend/app/components/charts/DeviationKpiPanel.vue
+++ frontend/app/components/charts/DeviationKpiPanel.vue
@@ -3,6 +3,7 @@
         <Card
             title="Écart à la normale moyen en France"
             :tooltip-text="`Écart à la normale moyen en France métropolitaine entre le ${formattedStart} et le ${formattedEnd}.`"
+            :loading="pending"
         >
             <template #kpi>
                 <p
@@ -24,8 +25,14 @@
         </Card>
 
         <Card
+<<<<<<< HEAD
             title="Nombre de jours au-dessus des normales"
             :tooltip-text="`Nombre de jours, entre le ${formattedStart} et le ${formattedEnd}, pour lesquels l'écart à la normale en France métropolitaine est supérieur à 0.`"
+=======
+            v-bind:loading="pending"
+            title="Jours au-dessus des normales"
+            tooltip-text="Nombre de jours, sur la période sélectionnée, pour lesquels l'écart à la normale en France est supérieur à 0."
+>>>>>>> b901e45 (Feat: DeviationKpiPanel  ajout etat loading)
         >
             <template #kpi>
                 <p class="font-semibold text-4xl mb-1 text-red-400">
@@ -39,8 +46,14 @@
         </Card>
 
         <Card
+<<<<<<< HEAD
             title="Nombre de jours en-dessous des normales"
             :tooltip-text="`Nombre de jours, entre le ${formattedStart} et le ${formattedEnd} pour lesquels l'écart à la normale en France métropolitaine est inférieur à 0.`"
+=======
+            v-bind:loading="pending"
+            title="Jours en-dessous des normales"
+            tooltip-text="Nombre de jours, sur la période sélectionnée, pour lesquels l'écart à la normale en France est inférieur à 0."
+>>>>>>> b901e45 (Feat: DeviationKpiPanel  ajout etat loading)
         >
             <template #kpi>
                 <p class="font-semibold text-4xl mb-1 text-blue-400">
@@ -83,5 +96,5 @@ const params = computed<NationalIndicatorKpiParams>(() => ({
     date_end: dateToStringYMD(pickedDateEnd.value),
 }));
 
-const { data: kpi } = useNationalIndicatorKpi(params);
+const { data: kpi, pending } = useNationalIndicatorKpi(params);
 </script>
