8e58af6f1bf5af514d93c96b6b2bb5ed7c974b90
Claire Dagan <claire.dagan.pro@gmail.com>
2026-05-13 14:02:25 +0200

    fix: stations: fix stations scroll

--- frontend/app/components/ui/commons/MaxTerritoriesReachedTooltip.vue
+++ frontend/app/components/ui/commons/MaxTerritoriesReachedTooltip.vue
@@ -12,7 +12,7 @@ defineProps<{ isAtLimit: boolean }>();
         :text="`${MAX_GRAPH_TERRITORIES} graphes maximum peuvent être affichés, veuillez supprimer un des choix sélectionné`"
         :content="{ align: 'center', side: 'top', sideOffset: 8 }"
     >
-        <div>
+        <div class="overflow-y-auto">
             <div :class="{ 'pointer-events-none opacity-50': isAtLimit }">
                 <slot />
             </div>
