0f14dd75589bfdecae4da821a828b41922fc9075
jlecordier <jeanlecordier@hotmail.fr>
2026-05-18 22:03:59 +0200

    clean code: back: conftest: names placeholders

--- backend/weather/tests/conftest.py
+++ backend/weather/tests/conftest.py
@@ -49,21 +49,20 @@ def insert_mv_record(
         cur.execute(
             """
             INSERT INTO public.mv_records_battus
-                (period_type, period_value, record_type,
-                 station_code, station_name, department,
-                 record_value, record_date)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                (period_type,     period_value,     record_type,     station_code,     station_name,     department,     record_value,     record_date)
+            VALUES
+                (%(period_type)s, %(period_value)s, %(record_type)s, %(station_code)s, %(station_name)s, %(department)s, %(record_value)s, %(record_date)s)
             """,
-            [
-                period_type,
-                period_value,
-                record_type,
-                station_code,
-                station_name,
-                department,
-                value,
-                date,
-            ],
+            {
+                "period_type": period_type,
+                "period_value": period_value,
+                "record_type": record_type,
+                "station_code": station_code,
+                "station_name": station_name,
+                "department": department,
+                "record_value": value,
+                "record_date": date,
+            },
         )
 
 
@@ -71,8 +70,11 @@ def set_cutoff(date: dt.date) -> None:
     with connection.cursor() as cur:
         cur.execute("TRUNCATE public.mv_records_battus_meta;")
         cur.execute(
-            "INSERT INTO public.mv_records_battus_meta (cutoff_date) VALUES (%s);",
-            [date],
+            """
+            INSERT INTO public.mv_records_battus_meta (cutoff_date)
+            VALUES (%(cutoff_date)s);
+            """,
+            {"cutoff_date": date},
         )
 
 
